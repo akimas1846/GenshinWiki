dc55afcb31cc14da175ddcccc96e94061a33dbd6
akimas1846 <akihiro0923092@gmail.com>
2024-01-25 14:34:07 +0900

    some change

--- src/components/sanctualVessel.jsx
+++ src/components/sanctualVessel.jsx
@@ -1,8 +1,34 @@
-export default function sanctualVessel() {
+import { useState } from "react";
+
+export default function SanctualVessel() {
+  const [parts, setParts] = useState("flower");
+  const [calcStatus, setCalcStatus] = useState("nomal");
+  function handlePartsChange(event) {
+    setParts(event.target.value);
+  }
+  function handleCalcChange(event) {
+    setCalcStatus(event.target.value);
+  }
   return (
     <div>
       <h2>聖遺物スコア計算機</h2>
-      <p>このページではゲーム内で獲得した聖遺物のスコアを計算します</p>
+      <label htmlFor="parts">部位を選択:</label>
+      <select id="category" onChange={handlePartsChange} value={parts}>
+        <option value="flower">花</option>
+        <option value="wing">羽</option>
+        <option value="clock">時計</option>
+        <option value="goblet">杯</option>
+        <option value="head">冠</option>
+      </select>
+      <br />
+      <label htmlFor="parts">ビルドを選択:</label>
+      <select id="category" onChange={handleCalcChange} value={calcStatus}>
+        <option value="normal">通常型</option>
+        <option value="hp">HP型</option>
+        <option value="wisdom">熟知型</option>
+        <option value="healer">ヒーラー</option>
+      </select>
+      <p>ゲーム内で獲得した聖遺物のスコアを計算します</p>
       <p>タブごとに、以下のように計算式を変えることが出来ます。</p>
       <dl>
         <ul>
